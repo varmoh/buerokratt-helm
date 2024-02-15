637cf6be4d32bfdb032f68508c9bd7e9d836d4a0
Varmo <101868197+varmoh@users.noreply.github.com>
2024-02-15 11:33:49 +0200

    Update update_chart.py

--- update_chart.py
+++ update_chart.py
@@ -1,5 +1,4 @@
 import os
-import fileinput
 
 def get_new_line_from_env(env_file_path):
     with open(env_file_path, 'r') as env_file:
@@ -9,16 +8,20 @@ def get_new_line_from_env(env_file_path):
     return None
 
 def update_chart_yaml(file_path, new_line):
+    # Read the contents of the file
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
     
-    for line_number, line_content in enumerate(fileinput.input(file_path, inplace=True), 1):
-        if line_number == 6:
-            print(new_line, end='')
-        else:
-            print(line_content, end='')
+    lines[5] = new_line
+
+    # Write the updated content back to the file
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
 
 # Example usage:
-env_file_path = '.env'
-chart_file_path = 'chat_backoffice/Chart.yaml' 
+env_file_path = '.env'  
+chart_file_path = 'chat_backoffice/Chart.yaml'  
 new_line = get_new_line_from_env(env_file_path)
 if new_line:
     print("New line to be added to Chart.yaml:", new_line)
@@ -29,3 +32,4 @@ if new_line:
         print("Error: Chart.yaml file not found.")
 else:
     print("Error: 'version' line not found in the .env file.")
+
