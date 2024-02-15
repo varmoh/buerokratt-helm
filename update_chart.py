036d8f743a5885f4a8e5ea98e2ef866cfa80faf5
Varmo <101868197+varmoh@users.noreply.github.com>
2024-02-15 10:36:54 +0200

    possbile pythn script fix

--- update_chart.py
+++ update_chart.py
@@ -13,23 +13,28 @@ def read_version_from_env():
 def update_chart_version(version):
     # Load Chart.yaml
     chart_file_path = os.path.join(os.getcwd(), 'chat_backoffice', 'Chart.yaml')
-    with open(chart_file_path, 'r') as chart_file:
-        chart_data = yaml.safe_load(chart_file)
+    try:
+        with open(chart_file_path, 'r') as chart_file:
+            chart_data = yaml.safe_load(chart_file)
 
-    # Update version
-    chart_data['version'] = version
+        # Update version
+        chart_data['version'] = version
 
-    # Write updated Chart.yaml
-    with open(chart_file_path, 'w') as chart_file:
-        yaml.dump(chart_data, chart_file)
+        # Write updated Chart.yaml
+        with open(chart_file_path, 'w') as chart_file:
+            yaml.dump(chart_data, chart_file)
+        
+        print(f"Chart version updated to {version}")
+    except Exception as e:
+        print(f"Error updating Chart version: {e}")
 
 def main():
     version = read_version_from_env()
     if version:
         update_chart_version(version)
-        print(f"Chart version updated to {version}")
     else:
         print("Version not found in .env file")
 
 if __name__ == "__main__":
     main()
+
